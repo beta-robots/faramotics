14ebdcbe71addf30ca5029398b839fbd148e3db1
andreucm <andreu@beta-robots.com>
2016-10-20 12:08:13 +0200

    Improved simple example with render call

--- src/examples/simple.cpp
+++ src/examples/simple.cpp
@@ -17,20 +17,21 @@ int main(int argc, char** argv)
     modelFileName = "/home/andreu/Desktop/Robotiq_F2_85_LD.stl";
     //eye_pose.setPose(200,100,100,M_PI,-0.6,0);
     eye = Eigen::AngleAxisd( M_PI, Eigen::Vector3d(0,0,1) );
-    eye.translation() = Eigen::Vector3d(200,100,100);     
-    std::cout << "main(): " << __LINE__ << std::endl; 
+    eye.translation() = Eigen::Vector3d(300,100,100);     
         
     //glut initialization
     faramotics::initGLUT(argc, argv);
-    std::cout << "main(): " << __LINE__ << std::endl; 
     
     //allocate faramotics object
     win = new Window(1200, 700, true, "faramotics_window");
     view = new CsceneRender(1200,700,90*M_PI/180.,90*(700./1200.)*(M_PI/180.),10.,600.,"view");
-    std::cout << "main(): " << __LINE__ << std::endl; 
+    view->loadAssimpModel(modelFileName);
+    view->printRenderParameters();
+    view->setViewPoint(eye);
+    view->render(); 
     
     //stop 1s
-    usleep(1000000);
+    usleep(3000000);
     
     //return
     delete win; 
